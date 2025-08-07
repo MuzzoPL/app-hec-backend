39cb09c323f45d31bbaf324fc573ac5a3624166c
MuzzoPL <167222651+MuzzoPL@users.noreply.github.com>
2025-08-06 21:18:28 -0300

    c

--- back-end/php-erp/beneficiarios/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/db.php';
-
-$stmt = $pdo->query("SELECT * FROM beneficiarios");
-$beneficiarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($beneficiarios);
-?>
